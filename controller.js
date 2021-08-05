98008973fcbb72f1f7ef58f6558b07f35ef6fb0f
Martin-Zurek <martin.zurek@education.nsw.gov.au>
2021-08-06 09:35:39 +1000

    Potential Fix
    I think I've fixed the mobile navigation menu.

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const navMenu = document.querySelector('.nav-menu');
   const navItems = document.querySelectorAll('.nav-item');
-  const buttons = document.querySelector('.nav-toggle');
+  const hamburger = document.querySelector('.nav-toggle');
 
   const toggle = e => e.classList.toggle('is-active');
   const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
