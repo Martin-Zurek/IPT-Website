e0874d73ca200416af672047c6f78816861e156f
Martin-Zurek <martin.zurek@education.nsw.gov.au>
2021-08-06 11:21:45 +1000

    Skills Section & Other Major Work
    Grabbed the skills section from the template (https://github.com/helfi92/studorlio/pull/11), and added some other stuff.

--- controller.js
+++ controller.js
@@ -2,10 +2,25 @@ window.onload = () => {
   const navMenu = document.querySelector('.nav-menu');
   const navItems = document.querySelectorAll('.nav-item');
   const hamburger = document.querySelector('.nav-toggle');
-  
+
   const toggle = e => e.classList.toggle('is-active');
   const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
 
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 }
+
+var delay = 500;
+$(".progress-bar").each(function (i) {
+  $(this).delay(delay * i).animate({ width: $(this).attr('aria-valuenow') + '%' }, delay);
+
+  $(this).prop('Counter', 0).animate({
+    Counter: $(this).text()
+  }, {
+    duration: delay,
+    easing: 'swing',
+    step: function (now) {
+      $(this).text(Math.ceil(now) + '%');
+    }
+  });
+});
